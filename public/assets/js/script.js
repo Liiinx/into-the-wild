881045aa3e347dc370dd26dcafb14724a2ea1048
gbrle <david.gabriele@outlook.fr>
2018-10-25 17:21:10 +0200

    Add effect HomePage slide, and JS file. Background changed.

---
+++ public/assets/js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+
+        $('a[href*=#portofolio]').click(function() {
+
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+                if (target.length) {
+
+                    $('html,body').animate({scrollTop: target.offset().top}, 1750);
+
+                    return false;
+
+                }
+
+            }
+
+        });
+
+    });
